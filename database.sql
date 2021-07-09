e938a0e7a9d52b7de0497d43cb5225a3de0f811a
Kashia Moua <kserenitym@gmail.com>
2021-07-09 10:43:27 -0500

    set up axios routes on app.jsx

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE items (
+"id" serial PRIMARY KEY,
+"item" varchar (80),
+"quantity" real,
+"unit" varchar (20),
+"purchase" boolean default false
+);
+
+INSERT INTO items (item, quantity, unit, purchase)
+VALUES ('apples','5', 'lb', 'false'),
+('carrots', '2', 'lb', 'true')
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,49 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import React from 'react';
-
 import Header from '../Header/Header.jsx'
 import './App.css';
 
-
 function App() {
+    let [itemList, setItemList] = useState([]);
+    let [newItemName, setNewItem] = useState('');
+    let [newItemQuant, setItemQuant]= useState('');
+    let [newItemUnitemList, setNewItemUnit] = useState('');
+    let [newItemPurchase, setNewItemPurchase] = useState('');
+
+    useEffect(() => {
+        console.log("in useEffect");
+        getItems();
+      }, []);
+    
+    // GET REQUEST
+    const getItems = () => {
+        axios({
+            method: 'GET',
+            url: '/list',
+          }).then( (response) => {
+            console.log(response)
+            console.log(response.data)
+            // setItemList(response.data);
+          }).catch( (error) => {
+            alert('Error getting items!')
+            console.log(error)
+          });
+        }
+  
+    //POST REQUEST
+    const addItem = () =>{
+        axios({
+            type: "POST",
+            url: '/list',
+            data: {
+                item: newItemName,
+                quantity: newItemQuant,
+                unit: newItemUnit,
+                purchase: newItemPurchase
+            }
+        })
+    }
     return (
         <div className="App">
             <Header />
